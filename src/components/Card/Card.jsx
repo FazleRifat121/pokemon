5c99d312ba0fd06b0d47def52aaacd3f4889563f
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-16 17:19:31 +0600

    fix the card size

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ const Card = ({ pokemon }) => {
   console.log(pokemon);
   return (
     <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
+      <div className="card bg-base-100 lg:w-96 w-full shadow-sm">
         <figure className="px-10 pt-10">
           <img
             src={pokemon.sprites.front_default}
